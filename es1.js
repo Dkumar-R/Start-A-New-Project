09c218943f5396a62d3bafce4c6f531989e74855
dk1402 <102382512+Dkumar-R@users.noreply.github.com>
2022-07-23 21:40:51 +0530

    es1

---
+++ es1.js
@@ -0,0 +1,6 @@
+let counter;
+console.log(counter);
+console.log(typeof counter);
+// let greeting = 'Hi';
+// let message  = "Bye";
+let message = 'I\'m also a valid string'; // use \ to escape the single quote (')
