e42c46441b9ab109dca89056a2dfcf758bd9824c
dk1402 <102382512+Dkumar-R@users.noreply.github.com>
2022-07-26 05:25:57 +0530

    Creating a constructors

---
+++ es2.js
@@ -0,0 +1,18 @@
+// let x = 10;
+// if (x == 10){
+//     let x = 20;
+//     console.log(x);
+// }
+// console.log((x));
+
+// var a = 10;
+// console.log(window.a);
+
+// let b = 20;
+// console.log(window.b);
+
+// for (var i = 0;i < 5; i++){
+//     setTimeout(function(){
+//         console.log(i);
+//     },2000)
+// }
